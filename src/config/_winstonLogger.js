631e966bb6ce8c3f9bfaeba991df9fa6fd273357
hossainchisty <hossain.chisty11@gmail.com>
2023-11-07 10:58:35 +0600

    format with prettier

--- src/config/_winstonLogger.js
+++ src/config/_winstonLogger.js
@@ -4,7 +4,7 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json(),
+    winston.format.json()
   ),
   transports: [
     new winston.transports.File({
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
-    }),
+    })
   );
 }
 
