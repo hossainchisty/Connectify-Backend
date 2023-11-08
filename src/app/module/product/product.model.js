fc10363ed4faba0de89414a8fef0b2e5dd15c665
hossainchisty <hossain.chisty11@gmail.com>
2023-11-08 19:48:43 +0600

    feat: added product schema

--- src/app/module/product/product.model.js
+++ src/app/module/product/product.model.js
@@ -2,13 +2,92 @@ const mongoose = require('mongoose');
 
 // Product Schema Definition
 
-const productSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    price: { type: Number, required: true }
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide product name"],
+    trim: true,
+    maxlength: [120, "Product name should not be more than 120 characters"],
   },
-  { timestamps: true},
-);
+  price: {
+    type: Number,
+    required: [true, "please provide product price"],
+    maxlength: [6, "Product price should not be more than 6 digits"],
+  },
+  description: {
+    type: String,
+    required: [true, "please provide product description"],
+  },
+  photos: [
+    {
+      id: {
+        type: String,
+        required: true,
+      },
+      secure_url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [
+      true,
+      "please select category from- short-sleeves, long-sleeves, sweat-shirts, hoodies",
+    ],
+    enum: {
+      values: ["shortsleeves", "longsleeves", "sweatshirt", "hoodies"],
+      message:
+        "please select category ONLY from - short-sleeves, long-sleeves, sweat-shirts and hoodies ",
+    },
+  },
+  stock: {
+    type: Number,
+    required: [true, "please add a number in stock"],
+  },
+  brand: {
+    type: String,
+    required: [true, "please add a brand for clothing"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Product = mongoose.model('Product', productSchema);
-module.exports = Product;
+module.exports = mongoose.model("Product", productSchema);
